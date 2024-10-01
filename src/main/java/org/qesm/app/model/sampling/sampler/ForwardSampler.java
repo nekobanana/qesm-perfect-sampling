c2b07057e867d085a2104c81cc1828179d725680
nekobanana <pru98@hotmail.it>
2024-10-01 20:54:03 +0200

    Revert "feat: N random numbers"
    This reverts commit fd570e8e34da6eb6e1affc598234e3f9f2a2145c.

--- src/main/java/org/qesm/app/model/sampling/sampler/ForwardSampler.java
+++ src/main/java/org/qesm/app/model/sampling/sampler/ForwardSampler.java
@@ -35,9 +35,11 @@ public class ForwardSampler extends Sampler {
 
     private StatesSnapshot generateNewSnapshot() {
         StatesSnapshot newStatesSnapshot = new StatesSnapshot();
+        int randomInt = rand.nextInt(n);
+        double randomDouble = rand.nextDouble();
         for (Map.Entry<Integer, State> entry : sequence.get(sequence.size() - 1).getStates().entrySet()) {
             State s;
-            int currentStateId = generateNextStateNumber(entry.getKey(), rand.nextInt(n), rand.nextDouble());
+            int currentStateId = generateNextStateNumber(entry.getKey(), randomInt, randomDouble);
             if (!newStatesSnapshot.getStates().containsKey(currentStateId)) {
                 s = new State();
                 s.setId(currentStateId);
